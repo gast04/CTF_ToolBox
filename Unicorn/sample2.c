a7235334175f95127f37d286a439e321f1649dee
Kurt Nistelberger <nistelberger@student.tugraz.at>
2018-08-01 23:29:27 +0200

    add unicorn capstone sample

---
+++ Unicorn/sample2.c
@@ -0,0 +1,29 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv) 
+{
+  
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+
+  int c = a*1234;
+  int d = b*4321;
+  
+  if ( a > 100) {
+    d = a+d;
+    c = b+c;
+  }
+  else {
+    d = a-d;
+    c = b-c;
+  }
+  d = d+c;
+
+  printf("%d\n", d);
+
+  return 0;
+}
+
